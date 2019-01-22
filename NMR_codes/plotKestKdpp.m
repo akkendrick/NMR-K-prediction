6441eec66e337cd2b19a495a821a443387070b1b
akkendrick <kentalex8@gmail.com>
2019-01-22 14:57:34 -0800

    Adding K_nmr K_dpp linear plotting codes and better profile

---
+++ NMR_codes/plotKestKdpp.m
@@ -0,0 +1,26 @@
+function plotKestKdpp(K,k_estimates,k_names,k_sym)
+    
+    figure
+    hold on
+    
+    grid on
+    box on
+    
+    plot(K,K,'LineWidth',2)
+    
+    [nrow, ncol] = size(k_estimates);
+    for a = 1:ncol
+        plot(K,k_estimates(:,a),k_sym{a},'MarkerSize',5)
+    end 
+    
+    legend(k_names)
+    
+    xlabel('DPP K (m/s)')
+    ylabel('SDR Estimated K (m/s)') 
+    
+    set(gca,'FontSize',14)
+    
+
+
+end
+
